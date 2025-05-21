9a9159392afcced2c0748719381a0f2874e9fc3e
ahamedshareef <shareef.006@gmail.com>
2025-05-21 19:55:09 +0530

    Search InStock Items

--- pwframework/tests/ecomm.spec.ts
+++ pwframework/tests/ecomm.spec.ts
@@ -6,10 +6,7 @@ test('open and check title', async({page}) => {
     await expect(page).toHaveTitle('Your Store')
 });
 
-
-
-
-test.only('Complete Registration', async({page}) => {
+test('Complete Registration', async({page}) => {
     await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=account/register');
     await page.locator('#input-firstname').fill('Ahamed');
     await page.locator("input[name='lastname']").fill('Shareef');
@@ -29,8 +26,29 @@ test.only('Complete Registration', async({page}) => {
 test('Trainman', async({page}) => {
     await page.goto('https://www.trainman.in/');
     await page.locator('//div[@text="Book Train Tickets"]').click();
+});
+
+test.only('Searching Instock Products ', async({page}) => {
+    await page.goto("https://ecommerce-playground.lambdatest.io/")
+    await page.locator(":text-is('Shop by Category')").click();
+    await page.locator(":text-is(' Phone, Tablets & Ipod')").click();
+    await page.locator("label[for=mz-fss-0--1]").click();
+
+    const grid = page.locator('div[data-view_id="grid"][class="row"]');
+    const products = grid.locator('.product-thumb');
+
+    const count = await products.count();
+    console.log("Product counts "+count);
+
+    for(let i=0;i<count;i++){
+        const eachProduct = products.nth(i).locator('.text-ellipsis-2');
+        const productName = await eachProduct.textContent();
+        console.log(`${i+1}th Product name is ${productName}`);
+    }
+
 
 
 });
+
 
 
